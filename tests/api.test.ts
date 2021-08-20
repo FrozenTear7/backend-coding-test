54addeef939b1ce38c834b247f02764aeaa07da4
Paweł Mendroch <pawelmendroch7@gmail.com>
2021-08-20 17:18:12 +0200

    Add coverage reports with nyc

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import sqlite3, { Database } from 'sqlite3';
 import buildSchemas from '../src/schemas';
 import buildAppWithDb from '../src/app';
+import { expect } from 'chai';
 
 const sqlite3Verbose = sqlite3.verbose();
 const db: Database = new sqlite3Verbose.Database(':memory:');
@@ -22,11 +23,10 @@ describe('API tests', () => {
   });
 
   describe('GET /health', () => {
-    it('should return health', async (done) => {
-      await request(app)
-        .get('/health')
-        .expect('Content-Type', /text/)
-        .expect(200, done);
+    it('should return health', async () => {
+      const res = await request(app).get('/health');
+      expect(res.statusCode).to.equal(200);
+      expect(res.type).to.equal('text/html');
     });
   });
 });
